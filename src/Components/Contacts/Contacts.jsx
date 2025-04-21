43ab65d8be5a581a2c19583fda493cb7d5f00b52
iftakhersifat <hossensifat615@gmail.com>
2025-04-22 00:42:17 +0600

    added error for contact us

---
+++ src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+const Contacts = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      
+      <div className="mt-32 text-center">
+        <h1 className="text-4xl font-bold text-red-500">404 Error: Contact Not Found</h1>
+        <p className="text-lg mt-4">Sorry, the Contact page you are looking for does not exist.</p>
+        <button
+          onClick={() => navigate('/')}
+          className="mt-6 px-6 py-2 border-2 border-[#176AE5] bg-[#176AE5] text-white rounded-full hover:bg-blue-600"
+        >
+          Go to Homepage
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Booking from './Components/Doctor/booking.jsx';
 import NotFound from './Components/NotFound/NotFound.jsx';
+import Contacts from './Components/Contacts/Contacts.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,7 +32,7 @@ const router = createBrowserRouter([
           const doctor = data.find(item => item.id === parseInt(params.doctorDetails));
           
           if (!doctor) {
-            return null; // Return null if doctor not found
+            return null;
           }
       
           return doctor;
@@ -44,6 +45,11 @@ const router = createBrowserRouter([
         path: "/booking/:id",
         Component: Booking
       },
+
+      {
+        path: "contactUs",
+        element: <Contacts></Contacts>, // Show Contact Not Found
+      }
       
     ]
   },
